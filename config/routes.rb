044990f37d36828c3cd8a8d7da88b8bc42a80f25
Teresa Martyny <teresamartyny@gmail.com>
2015-07-13 20:34:18 -0700

    remove rails comments for routes options. nest the plaid and stripe routes temporarily, ultimately they should be changed to reflect the type of resource rather than the api source of the resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,83 +1,26 @@
 Rails.application.routes.draw do
+  #visitors#index is for devise - This should be able to be deleted. No longer in use.
+  root to: 'visitors#index' 
 
   devise_for :users
-  resources :users
+  resources :users do
+    # I moved these routes in here to be nested
+    # however, the routes shouldn't be named according to the API the data comes from
+    # they need to be named for the resources themselves: ie: bank_charges, credit_charges.
+    resources :plaid, except: [:show, :index]
+    resources :stripe
+  end
   
   # CHARITIES
   patch '/charities/:id/users/:id/update' => 'charities#update', as: :update_charities_users
   get '/charities/:id/users/:id/index' => 'charities#index', as: :charities_users
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'sessions#index'
-  root to: 'visitors#index' #this is for devise
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  get 'plaid/new' => 'plaid#new'
-  patch 'plaid/create' => 'plaid#create'
-  get 'plaid/update' => 'plaid#edit'
-  patch 'plaid/update' => 'plaid#update'
-  get 'plaid/delete' => 'plaid#delete'
+  #DONATIONS GRAPH: This needs to be reorganized, it shouldn't be under plaid.
+  # It should be under a users or transactions controller
   get 'plaid/donations_graph' => 'plaid#donations_graph'
 
-  get 'stripe/new' => 'stripe#new'
-  post 'stripe/create' => 'stripe#create'
-  get '/stripe/update' => 'stripe#edit'
-  post '/stripe/update' => 'stripe#update'
-  get '/stripe/delete' => 'stripe#delete'
-
-
-  get '/settings/index' => 'settings#index'
+  #SETTINGS: These routes track user progress through initial sign up
+  # The unfinished signup controller should go under user controller.
   get '/settings/unfinished_signup' => 'settings#unfinished_signup'
 
-
-  
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
